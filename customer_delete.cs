d4277c041b941b557e2929ca3c62ba17abea3f96
Matt Buhagiar <mattbuhagiar@googlemail.com>
2011-03-08 23:27:03 +0000

    Created customer delete form, renamed labels and added tab index to the Delete and Cancel buttons

---
+++ customer_delete.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SOFT130Project
+{
+    public partial class customer_delete : Form
+    {
+        public customer_delete()
+        {
+            InitializeComponent();
+        }
+    }
+}
