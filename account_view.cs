2256efdb75fc01da9f941a1d2c36afb665a63a1d
Edward Marriner <edmarriner@gmail.com>
2011-03-10 16:37:19 +0000

    JOEL's Account view form

---
+++ account_view.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SOFT130Project.Resources
+{
+    public partial class account_view : Form
+    {
+        public account_view()
+        {
+            InitializeComponent();
+        }
+
+        private void account_view_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ account_view.designer.cs
@@ -0,0 +1,250 @@
+﻿namespace SOFT130Project.Resources
+{
+    partial class account_view
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCustomer = new System.Windows.Forms.Label();
+            this.lblAccountNumber = new System.Windows.Forms.Label();
+            this.lblAccountCaption = new System.Windows.Forms.Label();
+            this.lblAmountInAccount = new System.Windows.Forms.Label();
+            this.grpTransactions = new System.Windows.Forms.GroupBox();
+            this.lstTransactions = new System.Windows.Forms.ListBox();
+            this.btnSelectTransaction = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.grpTransactions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.SkyBlue;
+            this.pictureBox1.BackgroundImage = global::SOFT130Project.Properties.Resources.hmbc;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(244, 45);
+            this.pictureBox1.TabIndex = 121;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.SkyBlue;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(429, 69);
+            this.pictureBox2.TabIndex = 53;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.BackColor = System.Drawing.Color.SkyBlue;
+            this.lblDate.Location = new System.Drawing.Point(307, 9);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(30, 13);
+            this.lblDate.TabIndex = 122;
+            this.lblDate.Text = "Date";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BackColor = System.Drawing.Color.SkyBlue;
+            this.lblTime.Location = new System.Drawing.Point(307, 36);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(30, 13);
+            this.lblTime.TabIndex = 123;
+            this.lblTime.Text = "Time";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 124;
+            this.label1.Text = "Account view";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 125;
+            this.label2.Text = "Customer:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(64, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 126;
+            this.label3.Text = "Account:";
+            // 
+            // lblCustomer
+            // 
+            this.lblCustomer.AutoSize = true;
+            this.lblCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCustomer.Location = new System.Drawing.Point(143, 115);
+            this.lblCustomer.Name = "lblCustomer";
+            this.lblCustomer.Size = new System.Drawing.Size(93, 20);
+            this.lblCustomer.TabIndex = 127;
+            this.lblCustomer.Text = "lblCustomer";
+            // 
+            // lblAccountNumber
+            // 
+            this.lblAccountNumber.AutoSize = true;
+            this.lblAccountNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAccountNumber.Location = new System.Drawing.Point(143, 142);
+            this.lblAccountNumber.Name = "lblAccountNumber";
+            this.lblAccountNumber.Size = new System.Drawing.Size(139, 20);
+            this.lblAccountNumber.TabIndex = 128;
+            this.lblAccountNumber.Text = "lblAccountNumber";
+            // 
+            // lblAccountCaption
+            // 
+            this.lblAccountCaption.AutoSize = true;
+            this.lblAccountCaption.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAccountCaption.Location = new System.Drawing.Point(144, 171);
+            this.lblAccountCaption.Name = "lblAccountCaption";
+            this.lblAccountCaption.Size = new System.Drawing.Size(138, 20);
+            this.lblAccountCaption.TabIndex = 129;
+            this.lblAccountCaption.Text = "lblAccountCaption";
+            // 
+            // lblAmountInAccount
+            // 
+            this.lblAmountInAccount.AutoSize = true;
+            this.lblAmountInAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmountInAccount.Location = new System.Drawing.Point(144, 200);
+            this.lblAmountInAccount.Name = "lblAmountInAccount";
+            this.lblAmountInAccount.Size = new System.Drawing.Size(153, 20);
+            this.lblAmountInAccount.TabIndex = 130;
+            this.lblAmountInAccount.Text = "lblAmountInAccount";
+            // 
+            // grpTransactions
+            // 
+            this.grpTransactions.Controls.Add(this.btnSelectTransaction);
+            this.grpTransactions.Controls.Add(this.lstTransactions);
+            this.grpTransactions.Controls.Add(this.btnEdit);
+            this.grpTransactions.Location = new System.Drawing.Point(27, 240);
+            this.grpTransactions.Name = "grpTransactions";
+            this.grpTransactions.Size = new System.Drawing.Size(402, 174);
+            this.grpTransactions.TabIndex = 131;
+            this.grpTransactions.TabStop = false;
+            this.grpTransactions.Text = "Transactions";
+            // 
+            // lstTransactions
+            // 
+            this.lstTransactions.FormattingEnabled = true;
+            this.lstTransactions.Location = new System.Drawing.Point(6, 19);
+            this.lstTransactions.Name = "lstTransactions";
+            this.lstTransactions.Size = new System.Drawing.Size(390, 108);
+            this.lstTransactions.TabIndex = 0;
+            // 
+            // btnSelectTransaction
+            // 
+            this.btnSelectTransaction.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnSelectTransaction.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectTransaction.Location = new System.Drawing.Point(32, 133);
+            this.btnSelectTransaction.Name = "btnSelectTransaction";
+            this.btnSelectTransaction.Size = new System.Drawing.Size(154, 35);
+            this.btnSelectTransaction.TabIndex = 132;
+            this.btnSelectTransaction.Text = "Select transaction";
+            this.btnSelectTransaction.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(194, 133);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(155, 35);
+            this.btnEdit.TabIndex = 133;
+            this.btnEdit.Text = "Edit transaction";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            // 
+            // account_view
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 448);
+            this.Controls.Add(this.grpTransactions);
+            this.Controls.Add(this.lblAmountInAccount);
+            this.Controls.Add(this.lblAccountCaption);
+            this.Controls.Add(this.lblAccountNumber);
+            this.Controls.Add(this.lblCustomer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "account_view";
+            this.Text = "account_view";
+            this.Load += new System.EventHandler(this.account_view_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.grpTransactions.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCustomer;
+        private System.Windows.Forms.Label lblAccountNumber;
+        private System.Windows.Forms.Label lblAccountCaption;
+        private System.Windows.Forms.Label lblAmountInAccount;
+        private System.Windows.Forms.GroupBox grpTransactions;
+        private System.Windows.Forms.ListBox lstTransactions;
+        private System.Windows.Forms.Button btnSelectTransaction;
+        private System.Windows.Forms.Button btnEdit;
+    }
+}
